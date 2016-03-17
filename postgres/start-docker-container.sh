75b1fb7830939a3f1a382883712aac3be03f6f33
pavel-r <pavel-r@users.noreply.github.com>
2016-03-17 16:18:21 +0100

    Update start-docker-container.sh

--- postgres/start-docker-container.sh
+++ postgres/start-docker-container.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d -v /repository/other/seven/code/:/workspace/ --name postgres-seven -e POSTGRES_USER=postgres  postgres-seven
+docker run -d -v $PWD/code/:/workspace/ --name postgres-seven -e POSTGRES_USER=postgres  postgres-seven
