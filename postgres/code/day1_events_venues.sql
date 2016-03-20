ac7763736d3fe695de5af6bc909d27dbefe7cb4f
Pavel R <rabeckijps@gmail.com>
2016-03-20 13:54:04 +0000

    complete day 1

--- postgres/code/day1_events_venues.sql
+++ postgres/code/day1_events_venues.sql
@@ -64,3 +64,12 @@ select e.title, c.country_name from events e
 alter table venues add column active boolean default 'true';
 
 
+
+INSERT INTO events (title, starts, ends, venue_id) VALUES ( 'Moby' , '2012-02-06 21:00' , '2012-02-06 23:00' ,( SELECT venue_id FROM venues WHERE name = 'Crystal Ballroom' ) ); 
+
+INSERT INTO events (title, starts, ends, venue_id) VALUES ( 'Wedding' , '2012-02-26 21:00' , '2012-02-06 23:00' ,( SELECT venue_id FROM venues WHERE name = 'Voodoo Donuts' ) );
+
+INSERT INTO events (title, starts, ends, venue_id) VALUES ( 'Dinner with Mon' , '2012-02-26 18:00' , '2012-02-06 20:30' ,( SELECT venue_id FROM venues WHERE name = 'My Place' ) );
+
+INSERT INTO events (title, starts, ends) VALUES ( 'Valentines Day' , '2012-02-06 21:00' , '2012-02-06 23:00'); 
+
--- postgres/code/day2_grouping_functions.sql
+++
@@ -1,9 +0,0 @@
-
-INSERT INTO events (title, starts, ends, venue_id) VALUES ( 'Moby' , '2012-02-06 21:00' , '2012-02-06 23:00' ,( SELECT venue_id FROM venues WHERE name = 'Crystal Ballroom' ) ); 
-
-INSERT INTO events (title, starts, ends, venue_id) VALUES ( 'Wedding' , '2012-02-26 21:00' , '2012-02-06 23:00' ,( SELECT venue_id FROM venues WHERE name = 'Voodoo Donuts' ) );
-
-INSERT INTO events (title, starts, ends, venue_id) VALUES ( 'Dinner with Mon' , '2012-02-26 18:00' , '2012-02-06 20:30' ,( SELECT venue_id FROM venues WHERE name = 'My Place' ) );
-
-INSERT INTO events (title, starts, ends) VALUES ( 'Valentines Day' , '2012-02-06 21:00' , '2012-02-06 23:00'); 
-
